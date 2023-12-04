72abc03d69d8474672cc9c1b49204acc47601dc9
Maksim Glebov <32011520+st99-01-05@users.noreply.github.com>
2023-12-04 13:16:02 +0500

    Add files via upload

---
+++ Лаба2/laba2.2.py
@@ -0,0 +1,21 @@
+# Реализовать структуру данных «стек» (последний пришел, первый ушел) с помощью класса с возможностью просмотра, добавления и удаления элементов.
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def size(self):
+        return len(self.items)
